5b4578e1a9d58acbd65f1c62d3280e27b4d35c44
Peter Getek <postfixNotation@gmail.com>
2018-08-09 13:35:34 +0200

    added library files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # OLED_LIB_VGY12864L_03
-This repository contains an Arduino library for an OLED display module with the type number "VGY12864L-03"
+This repository contains an Arduino library for an OLED display module with the type number "VGY12864L-03".
---
+++ examples/OledExample/OledExample.ino
@@ -0,0 +1,21 @@
+#include <OLED_LIB_VGY12864L_03.h>
+// #include <image.h> // include this header of you
+// have an uin8_t array called "image" for instance
+// which contains data of an image
+
+// if you run this sketch 8 pixels or to be more precise
+// a display page is supposed to be set
+void setup() {
+  oled_spi.Init();
+  // oled_parallel.Init(); // this is how parallel inteface calls look like
+  oled_spi.SetContrastReg(0xFF); // 0x00 - 0xFF OR 0 - 255
+  oled_spi.SetPageValue(0, 0, 0xFF);
+  // oled_spi.DrawImage(image, sizeof(image) / sizeof(UI8));
+}
+
+// It is also recommended to look inside the respective header-file
+// since there are more comments that might help you deal with this
+// library.
+
+void loop() {
+}
